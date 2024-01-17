e56eb56f3347c4038696162edd5ab94b792272f1
SwampWood <danya.ney@yandex.ru>
2024-01-18 01:02:52 +0300

    База данных из dat файла почти готова

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 import csv
 import xlsxwriter
 import json
+import sqlite3
+from process import *
 
 with open('headers.json', encoding='UTF-8') as file:
     headers = json.load(file)
-    print(headers)
 
 
-def from_dat(file_dir):
-    content = pd.read_csv(file_dir, delim_whitespace=True, dtype=int, header=None)
-    content.columns = headers
-    print(content)
+def from_dat(file_dir, decode='Srok8c.ddl'):
+    all_, use_ = delim(decode)
+    with open(file_dir) as f:
+        data = []
+        data_type = [None] * len(all_)
+        for i in f.readlines():
+            row = []
+            cur = 0
+            for j in range(len(all_)):
+                idx = i[cur:cur + all_[j]][-use_[j]:]
+                idx = idx.replace(' ', '')
+                if data_type[j] is None and idx and '.' in idx:
+                    data_type[j] = 'REAL'
+                    row.append(float(idx))
+                elif data_type[j] is None and idx:
+                    data_type[j] = 'INT'
+                    row.append(int(idx))
+                else:
+                    row.append('NaN')
+                cur += all_[j]
+            data.append(row)
+            break
+    return data
 
 
-'''from_dat('forecasts/20046.dat')'''
+def to_sql(db):
+    conn = sqlite3.connect('temporary.db')
+    c = conn.cursor()
+
+    # Замените 'table_name' именем вашей таблицы
+    c.execute("CREATE TABLE IF NOT EXISTS table_name (column1 TEXT, column2 REAL)")
+
+    # Вставляем значения в таблицу
+    values = [(u'John', 21.0), (u'Jane', 19.0)]
+
+    for value in values:
+        c.execute('INSERT INTO table_name VALUES(?,?)', value)
+
+    # Сохраняем изменения
+    conn.commit()
+
+    # Закрываем соединение
+    conn.close()
+
+print(from_dat('forecasts/20046.dat'))
