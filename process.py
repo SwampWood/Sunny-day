f0be3b9f269a2bcd0d0feaffc21dc3fd60432818
SwampWood <danya.ney@yandex.ru>
2024-01-18 01:02:52 +0300

    База данных из dat файла почти готова

--- process.py
+++ process.py
@@ -1,21 +1,25 @@
-with open('Srok8c.ddl') as file:
-    s0 = file.readlines()[1:-1]
-    s = [i.strip().split('//')[-1][1:] for i in s0]
-    s2 = [i.split()[0:4] for i in s0]
-    print(*s, sep='\n')
-    sp = []
+access_name = None
+s = None
+s2 = None
+
+
+def access(name):
+    global s
+    global s2
+    global access_name
+    access_name = name
+    with open(name) as file:
+        s0 = file.readlines()[1:-1]
+        s = [i.strip().split('//')[-1][1:] for i in s0]
+        s2 = [i.split()[0:4] for i in s0]
+
+
+def delim(name):
+    if access_name != name:
+        access(name)
+    all_space = []
+    useful_space = []
     for i in s2:
-        if i[0] in ['KEY(I)', 'MIT']:
-            sp += [i[1]]
-        else:
-            if i[1] == 'Q':
-                sp += [i[0]]
-            elif i[1] == 'D':
-                sp += [i[0][:-1] + 'ЗНАК)']
-            elif i[1] == 'Q1':
-                sp += [i[0][:-1] + '1)']
-    print(sp)
-    sl = {}
-    for i in range(len(sp)):
-        sl[sp[i]] = s[i]
-    print(str(sl).replace('\'', '\"').replace(', ', ',\n'))
+        all_space += [int(i[2][3])]
+        useful_space += [int(i[3][3])]
+    return all_space, useful_space
